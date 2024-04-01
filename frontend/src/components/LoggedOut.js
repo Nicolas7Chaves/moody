01acd2c56e82feba259a3a43ede83ff4dbf5a2be
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-01 15:51:22 -0400

    add loggedout Component

---
+++ frontend/src/components/LoggedOut.js
@@ -0,0 +1,12 @@
+import './styles.scss'
+
+function LoggedOut() {
+    return (
+        <section>
+            <h1>test123
+            </h1>
+        </section>
+    )
+}
+
+export default LoggedOut;
