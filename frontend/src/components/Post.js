fe5395972b40df1fcfc25f7a2994a265bc3aed77
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-16 14:53:51 -0400

    add uid to database postings

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,21 +1,37 @@
 import './styles.scss'
 import { collection, addDoc } from "firebase/firestore";
-import { db } from '../firebase-config';
-import { useState } from 'react';
+import { db, auth } from '../firebase-config';
+import { useEffect, useState } from 'react';
 
 function Post(postBody) {
     const [postText, setPostText] = useState('');
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setUser(user);
+        });
+        return () => unsubscribe();
+    }, []);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const docRef = await addDoc(collection(db, "posts"), {
-                body: postText
-            });
-            console.log("Document written with ID: ", docRef.id);
-            setPostText('');
-        } catch (e) {
-            console.error("Error adding document: ", e);
+        if (user) {
+            try {
+                const docRef = await addDoc(collection(db, "posts"), {
+                    body: postText,
+                    uid: user.uid
+                });
+                console.log("Document written with ID: ", docRef.id);
+                setPostText('');
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
         }
+        else {
+            console.error("User not logged in")
+        }
+
     }
     return (
         <>
