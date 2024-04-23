1b67f2eee057d331fee4936ae938fc29e7a9b104
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-23 17:49:45 -0400

    add logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,8 @@
 import LogOut from './LogOut';
 import './styles.scss'
 import { useNavigate } from 'react-router-dom';
+import logopic from '../images/postr3.png';
+
 function Header() {
     const navigate = useNavigate();
 
@@ -13,6 +15,7 @@ function Header() {
     }
     return (
         <header className='header'>
+            <img className='header__logo' src={logopic} alt='logo' />
             <div className='header__divide'>
                 <a className='header__item' onClick={home}>Home</a>
                 <a className='header__item' onClick={myProfile} >Profile</a>
