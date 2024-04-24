229a3b6c40c8cba8000610eee7970425973eddd8
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-24 17:40:25 -0400

    add styling for filter buttons for phones and footer

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import GoogleSignIn from './GoogleSignIn';
 import logo from '../images/postrlogin.png';
+import Footer from './Footer';
 
 
 function LogIn() {
@@ -24,21 +25,24 @@ function LogIn() {
         }
     }
     return (
-        <section className='log-in'>
-            <img className='log-in__logo' src={logo} />
-            <form className='log-in__form' onSubmit={handleSubmit}>
-                <input className='log-in__input' name='email' type="email" placeholder="email" />
-                <input className='log-in__input' name='password' type="password" placeholder="password" />
-                <button className='log-in__button' type='submit'>
-                    Sign In
-                </button>
-            </form>
-            <div className='log-in__accounts-section'>
-                <button className='log-in__account' onClick={goToCreateAccount}>Create Account</button>
-                <GoogleSignIn />
-            </div>
+        <div className='log-in__section'>
+            <section className='log-in'>
+                <img className='log-in__logo' src={logo} />
+                <form className='log-in__form' onSubmit={handleSubmit}>
+                    <input className='log-in__input' name='email' type="email" placeholder="email" />
+                    <input className='log-in__input' name='password' type="password" placeholder="password" />
+                    <button className='log-in__button' type='submit'>
+                        Sign In
+                    </button>
+                </form>
+                <div className='log-in__accounts-section'>
+                    <button className='log-in__account' onClick={goToCreateAccount}>Create Account</button>
+                    <GoogleSignIn />
+                </div>
+            </section>
+            <Footer />
+        </div>
 
-        </section>
     )
 }
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Home from "../components/Home";
 
