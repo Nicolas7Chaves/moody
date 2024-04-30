eb15504f9f9495d16bb6d256bd14e3ea78f6ce50
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-04-29 21:02:24 -0400

    add .env folder for privacy firebase config

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -11,13 +11,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgS_yIZktdOEZMTq-blqohWi23KKMskDE",
-    authDomain: "moody-bc3b1.firebaseapp.com",
-    projectId: "moody-bc3b1",
-    storageBucket: "moody-bc3b1.appspot.com",
-    messagingSenderId: "18481814798",
-    appId: "1:18481814798:web:3cf8efc9eda8f178608b5f",
-    measurementId: "G-48LXZQYHL6"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -26,6 +27,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-console.log(db); 
+console.log(db);
 
 export { app, db };
